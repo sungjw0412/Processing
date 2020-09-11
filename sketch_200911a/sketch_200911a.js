fcb1dc723a7b200fdd8660a4065d3fa67eae6597
sungjw0412 <70360942+sungjw0412@users.noreply.github.com>
2020-09-11 14:40:12 +0900

    2020-09-11

---
+++ sketch_200911a/sketch_200911a.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500, 500);
+  
+  fill(250, 0, 0);
+  ellipse(100, 100, 50, 50);
+  fill(0, 0, 250);
+  ellipse(200, 200, 50, 50);
+  fill(250, 250, 0);
+  ellipse(150, 350, 50, 50);
+  fill(150, 250, 100);
+  ellipse(250, 250, 50, 50);
+  fill(100, 200, 0);
+  ellipse(150, 320, 50, 50);
+}
+
+
+function draw() {
+
+}
